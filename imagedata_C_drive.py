4c6163f192c37c5406c2b25c76e9118c59b64a4b
alpaBuddhabhatti <64379307+alpaBuddhabhatti@users.noreply.github.com>
2025-01-22 11:15:56 +0530

    Update imagedata_C_drive.py

--- imagedata_C_drive.py
+++ imagedata_C_drive.py
@@ -3,27 +3,30 @@ import base64
 from openai import AzureOpenAI
 
 # Set the directory to C drive test folder
-os.chdir(r"C:\Users\Jemin\repo\test")
+os.chdir(r"C:\Users\XXX\XXX\test")
 
 # Initialize Azure OpenAI client with key-based authentication
 client = AzureOpenAI(
-    azure_endpoint="https://azureopenai24demo.openai.azure.com/",
-    api_key="7c15f7bb4139435bbefa4b8e155a1622",
-    api_version="2024-05-01-preview",
+    azure_endpoint = "https://USEYOURAZUREOPENAI.openai.azure.com/",
+    api_key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    api_version = "XXXXXXXXXXXXXXXXXX",
 )
+  
+
 
 # Set image path using raw string to avoid escape sequence issues
-image_path = r"C:\Users\Jemin\repo\test\house.jpeg"
+image_path = r"C:\Users\XXX\XXX\XX\house.jpeg"
 
 # Convert image to base64
 with open(image_path, "rb") as image_file:
     base64_image = base64.b64encode(image_file.read()).decode("utf-8")
 
 # Use base64 encoded data URL
+#change jpeg to other image type if you have image with other then jpeg
 image_data_url = f"data:image/jpeg;base64,{base64_image}"
 
 completion = client.chat.completions.create(
-    model="gpt-40",
+    model="gpt-40", #Your Model name
     messages=[
         {
             "role": "user",
